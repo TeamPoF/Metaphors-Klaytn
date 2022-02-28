659c41055638b95d1bf2c0e1bbd3adfe6bf237ac
root <root@nowwater.opusm.io>
2022-02-27 19:13:35 -0600

    [Edit] edit port number

--- react/views/typescript/Api.ts
+++ react/views/typescript/Api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = `http://localhost:3000`;
+const url = `http://169.56.87.186:5500`;
 
 // Common ////////////////////////////////////////////////////////////
 
